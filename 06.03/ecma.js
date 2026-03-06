411a3b906222ead2d337c371ac0dd28ae6f124e4
Marcos Paulo <tenn77wolf@gmail.com>
2026-03-06 08:21:01 -0300

    ecma

--- 06.03/ecma.js
+++ 06.03/ecma.js
@@ -1,8 +1,10 @@
+var name = "Lucas";
+var idade = 30;
+var frase = "Olá, meu nome é " +
+             name +" e tenho " + 
+             idade + " anos.";
 
-
-
-
-
-
-
-
+const name = "Lucas";
+const idade = 30;
+const frase = `Olá, meu nome é 
+                ${name} e tenho ${idade} anos.`;
