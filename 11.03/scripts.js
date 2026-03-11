0bb04f69936703eae2ba7446f57cece26106c299
Marcos Paulo Alves Borges <tenn77wolf@gmail.com>
2026-03-11 12:16:16 -0300

    novo

---
+++ 11.03/scripts.js
@@ -0,0 +1,8 @@
+let botao = document.getElementById("botaoCor");
+botao.addEventListener('click', () => {
+    document.body.style.backgroundColor 
+        = "blue";
+    })
+let corAtual = 
+     document.body.style.backgroundColor;
+document.body.style.backgroundColor = "red";
